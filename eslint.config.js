209f486ace019cdfe3ea772f4c27b779372c5ec9
JiaxiangZhang <zjx9987@gmail.com>
2024-10-05 23:25:05 +0800

    🐳 chore: ignores template lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/template'] },
   { settings: { react: { version: '18.3.1' } } },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
