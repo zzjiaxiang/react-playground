91b1139fef31c4f84afa0c7ef63805b41c46c5c4
JiaxiangZhang <zjx9987@gmail.com>
2025-03-09 14:55:24 +0800

    🐳 chore: update react19

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@ant-design/v5-patch-for-react-19';
+
 import App from './App.tsx';
 import './index.css';
 
