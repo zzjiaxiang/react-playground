d8cdcdc419d6ff1acdb6e52ad8f5396fe6c2a04c
JiaxiangZhang <zjx9987@gmail.com>
2025-03-09 15:02:33 +0800

    Revert "🐳 chore: update react19"
    This reverts commit 91b1139fef31c4f84afa0c7ef63805b41c46c5c4.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@ant-design/v5-patch-for-react-19';
-
 import App from './App.tsx';
 import './index.css';
 
